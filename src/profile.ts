750305d4657306db42191bee1a80085b824d5b75
stuartt <34777184+RealMCoded@users.noreply.github.com>
2025-03-09 16:21:22 -0400

    Testing wearing items

--- src/profile.ts
+++ src/profile.ts
@@ -3,6 +3,7 @@
 import * as net from "net";
 import User from "./models/User";
 import { get_flag } from "./utils";
+import { item_id } from "./item-id";
 
 class PlayerProfile
 {
@@ -92,9 +93,26 @@ class PlayerProfile
         else
             username = Buffer.from(this.data.username + "\0", 'utf8')
 
-        //for now just send placeholder equip information.
-        let partItem = Buffer.alloc(2)
-        partItem.writeUInt16LE(0)
+        let equipped_items: Buffer[] = Array.from({ length: 13 }, () => {
+            const buf = Buffer.alloc(2);
+            buf.writeUInt16LE(0);
+            return buf;
+        });
+
+        /*
+        //this is my old outfit, for testing reasons.
+        equipped_items[1].writeUInt16LE(item_id.golden_egg_head)
+        equipped_items[0].writeUInt16LE(item_id.messy_brown_hair)
+        equipped_items[2].writeUInt16LE(item_id.golden_nightmare_scythe)
+        equipped_items[11].writeUInt16LE(item_id.black_wool_scarf)
+        equipped_items[4].writeUInt16LE(item_id.black_pants)
+        equipped_items[5].writeUInt16LE(item_id.black_shoes)
+        equipped_items[6].writeUInt16LE(item_id.diamond_cape)
+        equipped_items[7].writeUInt16LE(item_id.dark_sweater)
+        */
+
+        let itemEffect = Buffer.alloc(2)
+        itemEffect.writeUInt16LE(0)
 
         let visible = Buffer.alloc(1)
         visible.writeUInt8(+this.visible) // bool
@@ -114,22 +132,22 @@ class PlayerProfile
             countryB,
             jumps,
             username,
-            partItem, //Equipped 1
-            partItem, //Equipped 2
-            partItem, //Equipped 3
-            partItem, //Equipped 4
-            partItem, //Equipped 5
-            partItem, //Equipped 6
-            partItem, //Equipped 7
-            partItem, //Equipped 8
-            partItem, //Equipped 9
-            partItem, //Equipped 10
-            partItem, //Equipped 11
-            partItem, //Equipped 12
-            partItem, //Equipped 13
-            partItem, //ItemEffect 1
-            partItem, //ItemEffect 2
-            partItem, //ItemEffect 3
+            equipped_items[0], //Equipped 1
+            equipped_items[1], //Equipped 2
+            equipped_items[2], //Equipped 3
+            equipped_items[3], //Equipped 4
+            equipped_items[4], //Equipped 5
+            equipped_items[5], //Equipped 6
+            equipped_items[6], //Equipped 7
+            equipped_items[7], //Equipped 8
+            equipped_items[8], //Equipped 9
+            equipped_items[9], //Equipped 10
+            equipped_items[10], //Equipped 11
+            equipped_items[11], //Equipped 12
+            equipped_items[12], //Equipped 13
+            itemEffect, //ItemEffect 1
+            itemEffect, //ItemEffect 2
+            itemEffect, //ItemEffect 3
             visible,
             noclip,
             frozen
