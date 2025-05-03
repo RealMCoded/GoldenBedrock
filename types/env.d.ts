6d34adb5a47550f9a27cfb1c6e48e032ad54e4d8
stuartt <34777184+RealMCoded@users.noreply.github.com>
2025-05-03 17:31:07 -0400

    Create env.d.ts

---
+++ types/env.d.ts
@@ -0,0 +1,20 @@
+declare namespace NodeJS
+{
+    interface ProcessEnv
+    {
+        PORT: string;
+        PLAYER_CAP: string;
+        REGISTRATIONS_OPEN: "true" | "false";
+        IGNORE_VERSION: "true" | "false";
+        SWEAR_FILTER: "true" | "false";
+        CONSOLE_LOG: "true" | "false";
+        REPORTS_WEBHOOK: string;
+        SERVER_NAME: string;
+        BLACKLISTED_USER_NAMES: string;
+        BLACKLISTED_WORLD_NAMES: string;
+        RECOVERY_MAILER_SERVICE: string;
+        RECOVERY_MAILER_EMAIL: string;
+        RECOVERY_MAILER_DISPLAY_EMAIL: string;
+        RECOVERY_MAILER_PASSWORD: string;
+    }
+}
